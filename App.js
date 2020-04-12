6b5e2116dd345ca8ed0a1e6d57d5a8785e96ee2f
hai07890 <hai06780@gmail.com>
2020-04-12 22:17:05 +0700

    WarrantySearchDetail added

--- App.js
+++ App.js
@@ -2,66 +2,71 @@ import React from 'react';
 import { Text, View, ScrollView, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SearchBar, ListItem, Icon, Input } from 'react-native-elements';
 
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
 // function HomeScreen({ navigation }) {
-class HomeScreen extends React.Component {
+class WarrantySearch extends React.Component {
   state = {
-    'search': '',
-    'list': [],
+    search: '',
+    list: [],
   };
 
   updateSearch = search => {
     this.setState({
       'search': {search},
-      // 'list': [
-      //   {
-      //     title: 'Bom nuoc - BM001 - 25/05/2020',
-      //     icon: 'av-timer'
-      //   },
-      //   {
-      //     title: 'Hoa tien - AM002 - 25/05/2020',
-      //     icon: 'flight-takeoff'
-      //   },
-      //   {
-      //     title: 'Bom nuoc - BM001 - 25/05/2020',
-      //     icon: 'av-timer'
-      //   },
-      //   {
-      //     title: 'Hoa tien - AM002 - 25/05/2020',
-      //     icon: 'flight-takeoff'
-      //   },
-      //   {
-      //     title: 'Bom nuoc - BM001',
-      //     icon: 'av-timer'
-      //   },
-      //   {
-      //     title: 'Hoa tien - AM002',
-      //     icon: 'flight-takeoff'
-      //   },
-      //   {
-      //     title: 'Bom nuoc - BM001',
-      //     icon: 'av-timer'
-      //   },
-      //   {
-      //     title: 'Hoa tien - AM002',
-      //     icon: 'flight-takeoff'
-      //   },
-      //   {
-      //     title: 'Bom nuoc - BM001',
-      //     icon: 'av-timer'
-      //   },
-      //   {
-      //     title: 'Hoa tien - AM002',
-      //     icon: 'flight-takeoff'
-      //   },
-      // ],
+      'list': [
+        {
+          title: 'Bom nuoc - BM001 - 25/05/2020',
+          icon: 'av-timer'
+        },
+        {
+          title: 'Hoa tien - AM002 - 25/05/2020',
+          icon: 'flight-takeoff'
+        },
+        {
+          title: 'Bom nuoc - BM001 - 25/05/2020',
+          icon: 'av-timer'
+        },
+        {
+          title: 'Hoa tien - AM002 - 25/05/2020',
+          icon: 'flight-takeoff'
+        },
+        {
+          title: 'Bom nuoc - BM001',
+          icon: 'av-timer'
+        },
+        {
+          title: 'Hoa tien - AM002',
+          icon: 'flight-takeoff'
+        },
+        {
+          title: 'Bom nuoc - BM001',
+          icon: 'av-timer'
+        },
+        {
+          title: 'Hoa tien - AM002',
+          icon: 'flight-takeoff'
+        },
+        {
+          title: 'Bom nuoc - BM001',
+          icon: 'av-timer'
+        },
+        {
+          title: 'Hoa tien - AM002',
+          icon: 'flight-takeoff'
+        },
+      ],
     })
   };
 
   render() {
-    const { search } = this.state.search;
-    // const { list } = this.state.list
+    const { search } = this.state
+    const { list } = this.state
+    
 
     return (
       <View style={{ flex: 1}}>
@@ -79,9 +84,10 @@ class HomeScreen extends React.Component {
           ) 
         } */}
         <ScrollView>
-        {/* { 
+        { 
           list.map((item, i) => (
             <ListItem
+              onPress={() => this.props.navigation.navigate('WarrantySearchDetail')}
               key={i}
               title={item.title}
               leftIcon={{ name: item.icon }}
@@ -90,7 +96,7 @@ class HomeScreen extends React.Component {
               chevron
             />
           ))
-        } */}
+        }
         </ScrollView>
         {/* <Text>Product Name</Text>
         <Text>Product warranty code</Text>
@@ -108,7 +114,31 @@ class HomeScreen extends React.Component {
   }
 }
 
+function Home({}) {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen options={{headerShown: false}} name="WarrantySearch" component={WarrantySearch} />
+      <Stack.Screen name="WarrantySearchDetail" component={WarrantySearchDetail} />
+    </Stack.Navigator>
+  )
+};
+
+function WarrantySearchDetail() {
+  return (
+    <View>
+      <Text>Product Name</Text>
+      <Text>Product warranty code</Text>
+      <Text>Date Buying</Text>
+      <Text>Date Start active warranty</Text>
+      <Text>Date expire</Text>
+      <Text>Status</Text>
+      <Text>Note</Text>
+    </View>
+  );
+}
+
 function SettingsScreen({ navigation }) {
+  name: "detail"
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Input
@@ -127,8 +157,6 @@ function SettingsScreen({ navigation }) {
     </View>
   );
 }
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -155,19 +183,9 @@ export default function App() {
           inactiveTintColor: 'gray',
         }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={Home}/>
         <Tab.Screen name="Settings" component={SettingsScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-// export default function YourApp() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>
-//         Try editing me! 🎉
-//       </Text>
-//     </View>
-//   );
-// }
